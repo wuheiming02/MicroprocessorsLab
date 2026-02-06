c716aa8af59a1f0259bdb6c62ff397058e320c33
wuheiming02 <hilary.wu21@imperial.ac.uk>
2026-02-06 10:11:41 +0000

    06/02 first commit

--- main.s
+++ main.s
@@ -8,15 +8,17 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movlw 	0xFF
+	movwf	TRISD, A
+	movlw	0x0
+	clrf	TRISC, A
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+	movff 	0x06, PORTC
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movf 	PORTD, W, A
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
