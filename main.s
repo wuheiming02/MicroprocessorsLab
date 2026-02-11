df1f1cdadf1a93463d749c454bcc7d69885505ef
hilary <hhw221@ic.ac.uk>
2026-02-11 21:46:39 +0000

    Update main.s

--- main.s
+++ main.s
@@ -8,17 +8,15 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0xFF
-	movwf	TRISD, A
-	movlw	0x0
-	clrf	TRISC, A
+	movlw 	0x0
+	movwf	TRISB, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTC
+	movff 	0x06, PORTB
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movf 	PORTD, W, A
+	movlw 	0x63
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
