7a2cdba72f8b6e5b65d60353df20c715b593f17e
IC\hhw221 <hhw221@ic.ac.uk>
2026-02-24 12:05:34 +0000

    24/2 lab

--- ADC_mainA.s
+++ ADC_mainA.s
@@ -1,4 +1,4 @@
-; Main loop code: 
+; Main loop code:
 #include <xc.inc>
 
 ; Final
@@ -8,20 +8,17 @@ extrn   LCD_Send_Byte_I
 extrn   clear_LCD
 extrn   LCD_delay_ms
 
-extrn   Process_ADC     ; from adc_math.asm
+extrn   Process_ADC    
 extrn   ADC_Setup
 extrn   ADC_Read
-    
-extrn	DIG3, DIG2, DIG1, DIG0
+   
+extrn   DIG3
+extrn   DIG2
+extrn   DIG1
+extrn   DIG0
 
 global  rst
-    
-psect udata_acs
-
-;DIG3:       ds 1
-;DIG2:       ds 1
-;DIG1:       ds 1
-;DIG0:       ds 1
+   
 
 psect code, abs
 
@@ -40,8 +37,10 @@ main_loop:
     call    ADC_Read          ; take measurement
     call    Process_ADC       ; convert to DIG3?DIG0
 
-    call    clear_LCD
-
+    ; Move the cursor to the start to avoid LCD flicker from clear_LCD
+;    movlw   0x80
+;    call    LCD_Send_Byte_I
+    
     ; Thousands
     movf    DIG3, W, A
     addlw   0x30
@@ -65,12 +64,28 @@ main_loop:
     movf    DIG0, W, A
     addlw   0x30
     call    LCD_Send_Byte_D
-
+   
+    ; Space
+    movlw   ' '
+    ; call    LCD_Send_Byte_D
+   
+    ; Voltage unit V
+    movlw   'V'
+    ; call    LCD_Send_Byte_D
+    
+;    movf   ADRESL, W
+;    call    LCD_Send_Byte_D
+;    movf   ADRESH, W
+;    call    LCD_Send_Byte_D
+   
+    ; Delay
     movlw   200
     call    LCD_delay_ms      ; slow update
+    
+  
 
     goto    main_loop
-    
+   
 end
 
 
